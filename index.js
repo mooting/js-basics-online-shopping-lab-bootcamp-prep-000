f36d0fe797b1e70d07018e7f2ba7f8e5ca34db84
Justin Davis <justin_w_davis@yahoo.com>
2018-10-28 22:55:38 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -55,6 +55,6 @@ function placeOrder(cardNumber) {
   else{
     var returnTotal = total()
     cart = []
-    return `Your total cost is $${returnTotal}, which will be charged to the card ${ cardNumber }`
+    return `Your total cost is $${returnTotal}, which will be charged to the card ${ cardNumber }.`
   }
 }
