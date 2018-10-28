2cd8d135a99ca3e393fec4bae82b1add6a36f37a
Justin Davis <justin_w_davis@yahoo.com>
2018-10-28 22:44:25 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -43,12 +43,10 @@ function removeFromCart(item) {
     if( item === cart[i].itemName )
       cart.splice( i, 1 )
   }
-  if( oldLength !== cart.Length )
+  if( oldLength != cart.Length )
     return cart
   else
     return 'That item is not in your cart.'
-
-
 }
 
 function placeOrder(cardNumber) {
