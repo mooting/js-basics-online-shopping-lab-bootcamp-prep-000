c24570b65f86b4e7acdcd0af2d753b609e07934d
Justin Davis <justin_w_davis@yahoo.com>
2018-10-28 22:28:49 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ function viewCart() {
   if( cart.length > 0 ) {
     let cartString = `In your cart, you have ${ cart[0].itemName } at $${ cart[0].itemPrice }`
     for (let i = 1; i < cart.length - 1; i++)
-      cartString = `${ cartString }, ${ cart[i].itemName } at ${ cart[i].itemPrice }`
+      cartString = `${ cartString }, ${ cart[i].itemName } at $${ cart[i].itemPrice }`
 
     if( cart.length > 1)
       return `${ cartString }, and ${ cart[cart.length - 1].itemName } at $${ cart[cart.length - 1].itemPrice }.`
