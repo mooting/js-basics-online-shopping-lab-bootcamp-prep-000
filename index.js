e814dd7d83be07ca377753329e621bb504a6f2d1
Justin Davis <justin_w_davis@yahoo.com>
2018-10-28 22:31:11 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -31,7 +31,10 @@ function viewCart() {
 }
 
 function total() {
-  // write your code here
+  let total = 0
+  for( let i = 0; i < cart.length; i++ )
+    total = total + cart[i].itemPrice
+  return total  
 }
 
 function removeFromCart(item) {
