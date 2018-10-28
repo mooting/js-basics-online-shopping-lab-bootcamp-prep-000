9ee982c78d63d5ad22a2c8e44105511c6d5b9b88
Justin Davis <justin_w_davis@yahoo.com>
2018-10-28 22:52:11 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -50,5 +50,11 @@ function removeFromCart(item) {
 }
 
 function placeOrder(cardNumber) {
-  // write your code here
+  if( cardNumber === undefined )
+    return `Sorry, we don't have a credit card on file for you.`
+  else{
+    let total = total()
+    cart = []
+    return total
+  }  
 }
